40aefcf3aa8cdb1c4c67675a9896e801fb8dcdd1
y15e <y15e@qq.com>
2023-10-17 15:55:35 +0900

    Add basic auth

--- server.js
+++ server.js
@@ -14,7 +14,25 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json())
-//app.use(express.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  
+  // Basic Auth
+  const auth = {
+    login: process.env.BASIC_AUTH_LOGIN,
+    password: process.env.BASIC_AUTH_PASSWORD
+  }
+
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  
+  if (login && password && login === auth.login && password === auth.password) {
+    return next()
+  }
+
+  res.set('WWW-Authenticate', 'Basic realm="401"')
+  res.status(401).send('Authentication required.')
+})
 
 app.get("/:merchant", async (req, res) => {
   
